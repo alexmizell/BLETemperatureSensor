49a793f983558885dcf9c04844c8a46fa1017970
alex <alexmizell@gmail.com>
2023-07-08 16:17:08 -0400

    First commit

---
+++ BLETemperatureSensor/Program.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.Advertisement;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Devices.Enumeration;
+using Windows.Storage.Streams;
+
+namespace BLETemperatureSensor
+{
+    internal class Program
+    {
+
+        static DeviceInformation device = null;
+        static async Task Main(string[] args)
+        {
+
+            Boolean notified = false;
+
+            // Query for extra properties you want returned
+            string[] requestedProperties = { "System.Devices.Aep.DeviceAddress", "System.Devices.Aep.IsConnected" };
+
+            DeviceWatcher deviceWatcher =
+                        DeviceInformation.CreateWatcher(
+                                BluetoothLEDevice.GetDeviceSelectorFromPairingState(false),
+                                requestedProperties,
+                                DeviceInformationKind.AssociationEndpoint);
+
+            // Register event handlers before starting the watcher.
+            // Added, Updated and Removed are required to get all nearby devices
+            deviceWatcher.Added += DeviceWatcher_Added;
+            deviceWatcher.Updated += DeviceWatcher_Updated;
+            deviceWatcher.Removed += DeviceWatcher_Removed;
+
+            // EnumerationCompleted and Stopped are optional to implement.
+            deviceWatcher.EnumerationCompleted += DeviceWatcher_EnumerationCompleted;
+            deviceWatcher.Stopped += DeviceWatcher_Stopped;
+
+            // Start the watcher.
+            deviceWatcher.Start();
+
+            while (true)
+            {
+                if (device == null)
+                {
+                    Thread.Sleep(200);
+                }
+                else
+                {
+
+                    if (!notified)
+                    {
+                        //Console.WriteLine("Press \"any\" key to pair with IBS-TH2...");
+                        //Console.ReadKey();
+
+                        BluetoothLEDevice bluetoothLeDevice = await BluetoothLEDevice.FromIdAsync(device.Id);
+
+                        Console.WriteLine("Attempting to pair with device");
+                        GattDeviceServicesResult result = await bluetoothLeDevice.GetGattServicesAsync();
+
+                        if (result.Status == GattCommunicationStatus.Success)
+                        {
+                            Console.WriteLine("Pairing succeeded");
+                            var services = result.Services;
+                            foreach (var service in services)
+                            {
+
+                                //IReadOnlyList<GattCharacteristic> characteristics2 = service.GetAllCharacteristics();
+
+                                //foreach(var characteristic in characteristics2)
+                                //{
+                                //    //Console.WriteLine("UUID: " + service.Uuid.ToString());
+                                //    Console.WriteLine("Characteristic:  " + characteristic.CharacteristicProperties.ToString());
+
+                                //}
+
+                                Console.WriteLine("Service UUID: " + service.Uuid);
+                                //Console.WriteLine("UUID: " + service.ToString());
+                                //Console.WriteLine("UUID: " + service.Session.ToString());
+                                //Console.WriteLine("UUID: " + service.Device.ToString());
+
+                                //if (service.Uuid.ToString() == "00001801-0000-1000-8000-00805f9b34fb")  // {00002a05-0000-1000-8000-00805f9b34fb}
+                                //{
+                                //    Console.WriteLine("Found temperature service...");
+                                GattCharacteristicsResult charactiristicResult = await service.GetCharacteristicsAsync();
+
+                                if (charactiristicResult.Status == GattCommunicationStatus.Success)
+                                {
+                                    var characteristics = charactiristicResult.Characteristics;
+                                    foreach (var characteristic in characteristics)
+                                    {
+                                        //Console.WriteLine(characteristic);
+                                        GattCharacteristicProperties properties = characteristic.CharacteristicProperties;
+
+                                        if (properties.HasFlag(GattCharacteristicProperties.Read))
+                                        {
+                                            // This characteristic supports reading from it.
+                                            Console.WriteLine("       " + characteristic.Uuid + " " + characteristic.UserDescription + " supports reading.");
+
+                                            var characteristicUUID = characteristic.Uuid.ToString().Remove(8).Remove(0, 4);
+
+                                            if (characteristicUUID == "" ||
+                                                characteristicUUID == "" ||
+                                                characteristicUUID == "" ||
+                                                characteristicUUID == "" ||
+                                                characteristicUUID == "" ||
+                                                characteristicUUID == "" ||
+                                                characteristicUUID == "" ||
+                                                characteristicUUID == "" ||
+                                                characteristicUUID == "fff1" ||
+                                                characteristicUUID == "fff2" ||
+                                                characteristicUUID == "fff3" ||
+                                                characteristicUUID == "fff4" ||
+                                                characteristicUUID == "fff5" ||
+                                                characteristicUUID == "fff6" ||
+                                                characteristicUUID == "fff7" ||
+                                                characteristicUUID == "fff8" ||
+                                                characteristicUUID == "")
+                                            {
+
+                                                GattReadResult result2 = await characteristic.ReadValueAsync();
+                                                if (result.Status == GattCommunicationStatus.Success)
+                                                {
+                                                    var reader = DataReader.FromBuffer(result2.Value);
+                                                    byte[] input = new byte[reader.UnconsumedBufferLength];
+
+
+                                                    //reader.ReadBytes(input);
+
+                                                    // The encoding and byte order need to match the settings of the writer 
+                                                    // we previously used.
+                                                    reader.UnicodeEncoding = Windows.Storage.Streams.UnicodeEncoding.Utf8;
+                                                    //reader.ByteOrder = Windows.Storage.Streams.ByteOrder.LittleEndian;
+
+                                                    // Once we have written the contents successfully we load the stream.
+                                                    //await reader.LoadAsync((uint)stream.Size);
+
+                                                    //var receivedStrings = "";
+
+                                                    // Keep reading until we consume the complete stream.
+                                                    while (reader.UnconsumedBufferLength > 0)
+                                                    {
+                                                        // Note that the call to readString requires a length of "code units" 
+                                                        // to read. This is the reason each string is preceded by its length 
+                                                        // when "on the wire".
+
+                                                        byte[] bytesToRead = new byte[reader.UnconsumedBufferLength];
+
+                                                        reader.ReadBytes(bytesToRead);
+
+                                                        foreach (byte b in bytesToRead)
+                                                        {
+                                                            char thisChar = (char)b;
+
+                                                            Console.Write($"{thisChar}");
+
+
+                                                        }
+                                                        Console.WriteLine("");
+                                                        foreach (byte b in bytesToRead)
+                                                        {
+                                                            string hexValue = b.ToString("X2");
+
+                                                            Console.Write($"{hexValue} ");
+
+
+                                                        }
+                                                        Console.WriteLine("");
+                                                        foreach (byte b in bytesToRead)
+                                                        {
+
+                                                            int decimalValue = (int)b;
+
+                                                            Console.Write($"{decimalValue}, ");
+
+                                                        }
+                                                        Console.WriteLine("");
+                                                    }
+
+                                                    //Console.WriteLine(receivedStrings);
+                                                    Console.WriteLine("");
+                                                }
+
+                                            }
+
+                                        }
+                                        if (properties.HasFlag(GattCharacteristicProperties.Write))
+                                        {
+                                            // This characteristic supports writing to it.
+                                            Console.WriteLine("       " + characteristic.Uuid + " " + characteristic.UserDescription + " supports writing.");
+                                        }
+                                        if (properties.HasFlag(GattCharacteristicProperties.Notify))
+                                        {
+                                            // This characteristic supports subscribing to notifications.
+                                            Console.WriteLine("       " + characteristic.Uuid + " " + characteristic.UserDescription + " supports notifications.");
+
+
+                                            GattCommunicationStatus status = await characteristic.WriteClientCharacteristicConfigurationDescriptorAsync(
+                                            GattClientCharacteristicConfigurationDescriptorValue.Notify);
+
+                                            if (status == GattCommunicationStatus.Success)
+                                            {
+
+                                                Console.WriteLine("subscribed");
+                                                Console.WriteLine("");
+                                                characteristic.ValueChanged += Characteristic_ValueChanged;
+                                                // Server has been informed of clients interest.
+                                            }
+
+                                        }
+
+
+
+
+                                    }
+                                }
+
+                                Console.WriteLine("");
+
+                            }
+                        }
+
+
+
+                        notified = true;
+
+                    }
+                    else // we are signed up for notifications
+                    {
+
+                        Console.WriteLine("waiting for notifications...");
+                        Console.WriteLine("");
+
+                        while (true)
+                        {
+                            Console.WriteLine("Press Any Key to Exit application");
+                            Console.ReadKey();
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void DeviceWatcher_Stopped(DeviceWatcher sender, object args)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private static void DeviceWatcher_EnumerationCompleted(DeviceWatcher sender, object args)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private static void DeviceWatcher_Removed(DeviceWatcher sender, DeviceInformationUpdate args)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private static void DeviceWatcher_Updated(DeviceWatcher sender, DeviceInformationUpdate args)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private static void DeviceWatcher_Added(DeviceWatcher sender, DeviceInformation args)
+        {
+            //Console.WriteLine(args.Id);
+            //Console.WriteLine(args.Kind.ToString());
+            //Console.WriteLine("");
+
+            if (args.Id.Contains("49:22:07:07:03:b3"))
+            {
+                Console.WriteLine("Found the IBS-TH2 by MAC address.");
+
+                device = args;
+
+            }
+
+             
+        }
+
+        private static void Characteristic_ValueChanged(GattCharacteristic sender, GattValueChangedEventArgs args)
+        {
+            var reader = DataReader.FromBuffer(args.CharacteristicValue);
+            var flags = reader.ReadByte();
+            var value = reader.ReadByte();
+            Console.WriteLine($"{flags} - {value}");
+        }
+
+    }
+}
---
+++ BLETemperatureSensor2/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.Storage.Streams;
+using Windows.Devices.Bluetooth.Advertisement;
+using System.Text.RegularExpressions;
+
+//using SDKTemplate;
+
+namespace BLETemperatureSensor2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BluetoothLEAdvertisementWatcher watcher = new BluetoothLEAdvertisementWatcher();
+
+            watcher.ScanningMode = BluetoothLEScanningMode.Active;
+
+            // Attach a handler to process the received advertisement. 
+            // The watcher cannot be started without a Received handler attached
+
+            watcher.Received += OnAdvertisementReceived;
+
+            // Attach a handler to process watcher stopping due to various conditions,
+            // such as the Bluetooth radio turning off or the Stop method was called
+
+            watcher.Stopped += OnAdvertisementWatcherStopped;
+
+            watcher.Start();
+
+            while (true)
+            {
+
+            }
+        }
+
+        private static async void OnAdvertisementReceived(BluetoothLEAdvertisementWatcher watcher, BluetoothLEAdvertisementReceivedEventArgs eventArgs)
+        {
+            // We can obtain various information about the advertisement we just received by accessing 
+            // the properties of the EventArgs class
+
+            // The timestamp of the event
+            DateTimeOffset timestamp = eventArgs.Timestamp;
+
+            // The type of advertisement
+            BluetoothLEAdvertisementType advertisementType = eventArgs.AdvertisementType;
+
+            // The received signal strength indicator (RSSI)
+            Int16 rssi = eventArgs.RawSignalStrengthInDBm;
+
+            // The local name of the advertising device contained within the payload, if any
+            string localName = eventArgs.Advertisement.LocalName;
+
+            // Check if there are any manufacturer-specific sections.
+            // If there is, print the raw data of the first manufacturer section (if there are multiple).
+            string manufacturerDataString = "";
+            var manufacturerSections = eventArgs.Advertisement.ManufacturerData;
+
+            var isScannable = eventArgs.IsScannable;
+            var isScanReponse = eventArgs.IsScanResponse;
+            var bluetoothAddress = eventArgs.BluetoothAddress;
+            var blutetoothAddresType = eventArgs.BluetoothAddressType;
+            var isDirected = eventArgs.IsDirected;
+            var isAnon = eventArgs.IsAnonymous;
+            Guid UUID = Guid.Empty;
+
+            ulong input = bluetoothAddress;
+            var tempMac = input.ToString("X");
+            //tempMac is now 'E7A1F7842F17'
+
+            var regex = "(.{2})(.{2})(.{2})(.{2})(.{2})(.{2})";
+            var replace = "$1:$2:$3:$4:$5:$6";
+            var macAddress = Regex.Replace(tempMac, regex, replace);
+
+
+            // if (eventArgs.Advertisement.ServiceUuids.Count > 0 ) { UUID = eventArgs.Advertisement.ServiceUuids[0]; }
+
+            //if (manufacturerSections.Count > 0)
+
+            var temperature = 0f;
+            var battery = 0;
+            var humidity = 0f;
+            
+            foreach(var section in manufacturerSections)
+            {
+                // Only print the first one of the list
+
+                // UUID = eventArgs.Advertisement.ManufacturerData[0].
+                
+                var manufacturerData = section;
+                var data = new byte[manufacturerData.Data.Length];
+                using (var reader = DataReader.FromBuffer(manufacturerData.Data))
+                {
+                    reader.ReadBytes(data);
+                }
+
+                if(data.Length == 7)
+                {
+                    // Print the company ID + the raw data in hex format
+                    manufacturerDataString = string.Format("0x{0}: length {1} : {2}",
+                        manufacturerData.CompanyId.ToString("X4"),
+                        manufacturerData.Data.Length,
+                        BitConverter.ToString(data));
+                    temperature = ((float)manufacturerData.CompanyId) / 100f;
+                    humidity = ((data[1] << 8) + data[0]) / 100.0f;
+                    battery = data[5];
+                }
+
+            }
+
+            //if (advertisementType == BluetoothLEAdvertisementType.ScanResponse)
+            //{
+            // Display these information on the list
+            //Console.WriteLine(string.Format("[{0}]: rssi={1}, bluetoothAddress=[{3}], isScannable=[{4}], name={6}], type={2}, MSD=[{5}]",
+            //    timestamp.ToString("hh\\:mm\\:ss\\.fff"),
+            //    rssi.ToString(),
+            //    advertisementType.ToString(),
+            //    macAddress.ToString(),
+            //    isScannable.ToString(),
+            //    manufacturerDataString, 
+            //    localName,
+            //    UUID.ToString()
+            //    ));
+            //}
+
+
+            if (macAddress == "49:22:07:07:03:B3" && advertisementType == BluetoothLEAdvertisementType.ScanResponse)
+            {
+                Console.WriteLine(string.Format("[{0}]: bluetoothAddress=[{1}], temperature=[{2}], humidity=[{3}], battery=[{4}], manufacturerDataString=[{5}]",
+                        timestamp.ToString("hh\\:mm\\:ss\\.fff"),
+                        macAddress,
+                        temperature,
+                        humidity,
+                        battery,
+                        manufacturerDataString
+                        //advertisementType.ToString()
+                        //rssi.ToString(),
+                        //localName,
+                        //compa
+                        //isScannable.ToString()
+                        )); ;
+            }
+
+        }
+
+        /// <summary>
+        /// Invoked as an event handler when the watcher is stopped or aborted.
+        /// </summary>
+        /// <param name="watcher">Instance of watcher that triggered the event.</param>
+        /// <param name="eventArgs">Event data containing information about why the watcher stopped or aborted.</param>
+        private static async void OnAdvertisementWatcherStopped(BluetoothLEAdvertisementWatcher watcher, BluetoothLEAdvertisementWatcherStoppedEventArgs eventArgs)
+        {
+
+                Console.WriteLine(string.Format("Watcher stopped or aborted: {0}", eventArgs.Error.ToString()));
+
+        }
+    }
+}
